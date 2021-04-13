4af76e000c294866c325838362e1c9ddb776544b
Santiago <dansan150599@gmail.com>
2021-04-13 14:39:24 -0500

    initial commit

---
+++ app/src/main/assets/project.js
@@ -0,0 +1,98 @@
+// JSON file describing the AR-experience for offline usage
+var projectJSONOffline =
+{
+    "isCloud": false,
+    "tcId": "6075c0a519a83275e546cc88",
+    "name": "RA",
+    "targets": [
+        {
+            "name": "H2O",
+            "augmentations": [
+                {
+                    "height": 0.1,
+                    "type": "Label",
+                    "targetId": "6075de96fafcef75dca86e29",
+                    "minSDK": "6.1.0",
+                    "modDat": 1618339609728,
+                    "creDat": 1618338383982,
+                    "properties": {
+                        "name": "label_1",
+                        "text": "Composicion del agua",
+                        "style": {
+                            "backgroundColor": "rgba(0,0,0,1)",
+                            "fontStyle": "normal",
+                            "textColor": "rgba(255,255,255,1)"
+                        },
+                        "ignoreInAR": false,
+                        "opacity": 1,
+                        "rotate": {
+                            "x": 0,
+                            "y": 0,
+                            "z": 0
+                        },
+                        "scale": {
+                            "x": 1,
+                            "y": 1,
+                            "z": 1
+                        },
+                        "translate": {
+                            "x": -0.0623656684966622,
+                            "y": 0.7378900070274661,
+                            "z": 0.16252635787315128
+                        },
+                        "zOrder": 0
+                    },
+                    "fileSize": 0,
+                    "id": "6075e24fa98a6075e1b212c6"
+                },
+                {
+                    "type": "ImageDrawable",
+                    "height": 0.3,
+                    "uri": "augmentation-assets/70821_H2O/6075e6dba98a6075e1b212dd/image.jpeg",
+                    "targetId": "6075de96fafcef75dca86e29",
+                    "minSDK": "6.1.0",
+                    "modDat": 1618339605762,
+                    "creDat": 1618339547940,
+                    "properties": {
+                        "name": "h2o",
+                        "forceNativeBrowser": "false",
+                        "ignoreInAR": false,
+                        "opacity": 1,
+                        "rotate": {
+                            "x": 0,
+                            "y": 0,
+                            "z": 0
+                        },
+                        "scale": {
+                            "x": 1,
+                            "y": 1,
+                            "z": 1
+                        },
+                        "translate": {
+                            "x": 0.06295066131030747,
+                            "y": 0.24297598753968752,
+                            "z": -0.15938435938731876
+                        },
+                        "zOrder": 1
+                    },
+                    "fileSize": 16979,
+                    "id": "6075e6dba98a6075e1b212dd"
+                }
+            ]
+        }
+    ],
+    "type": "ObjectTargetCollection",
+    "wtoFiles": [
+        {
+            "nrOfTargets": 1,
+            "version": "8.0",
+            "url": "augmentation-trackers/8.0/tracker.wto",
+            "creDat": 1618337461343
+        }
+    ],
+    "sdkVersion": "8.0",
+    "settings": {
+        "tracker": {}
+    }
+}
+;
